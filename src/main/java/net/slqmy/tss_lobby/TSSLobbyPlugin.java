c8d1e286def966366554648e96e3a25907bd6e25
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2023-08-06 16:54:54 +0100

    📋 Created basic plugin features.

--- src/main/java/net/slqmy/tss_lobby/TSSLobbyPlugin.java
+++ src/main/java/net/slqmy/tss_lobby/TSSLobbyPlugin.java
@@ -1,10 +1,22 @@
 package net.slqmy.tss_lobby;
 
+import net.slqmy.tss_core.TSSCorePlugin;
+import net.slqmy.tss_lobby.event.JoinListener;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class TSSLobbyPlugin extends JavaPlugin {
 
+	private final TSSCorePlugin core = (TSSCorePlugin) Bukkit.getPluginManager().getPlugin("TSS-Core");
+
+	public TSSCorePlugin getCore() {
+		return core;
+	}
+
 	@Override
 	public void onEnable() {
+		getDataFolder().mkdir();
+
+		Bukkit.getPluginManager().registerEvents(new JoinListener(this), this);
 	}
 }
