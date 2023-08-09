d9729f1adddfd78a90fed927be9ad9cd59d788ee
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2023-08-09 17:10:32 +0100

    🧨 Added spiraling particles to fireworks.

--- src/main/java/net/slqmy/tss_lobby/event/JoinListener.java
+++ src/main/java/net/slqmy/tss_lobby/event/JoinListener.java
@@ -7,12 +7,15 @@ import net.slqmy.tss_core.data.type.Rank;
 import net.slqmy.tss_core.manager.MessageManager;
 import net.slqmy.tss_lobby.TSSLobbyPlugin;
 import org.bukkit.FireworkEffect;
+import org.bukkit.Particle;
+import org.bukkit.World;
 import org.bukkit.entity.Firework;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.meta.FireworkMeta;
+import org.bukkit.scheduler.BukkitRunnable;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Random;
@@ -38,17 +41,18 @@ public class JoinListener implements Listener {
 		player.sendTitlePart(TitlePart.SUBTITLE, messageManager.getPlayerMessage(Message.WELCOME_SUBTITLE, player));
 
 		Rank rank = plugin.getRanksPlugin().getRankManager().getPlayerRank(player);
+		World playerWorld = player.getWorld();
 
 		for (int i = 0; i < 5; i++) {
-			final Firework firework = player.getWorld().spawn(
+			Firework firework = playerWorld.spawn(
 							player.getLocation().clone().add(
 											random.nextDouble(-1D, 1D),
-											1,
+											2,
 											random.nextDouble(-1D, 1D)),
 							Firework.class
 			);
 
-			final FireworkMeta fireworkMeta = firework.getFireworkMeta();
+			FireworkMeta fireworkMeta = firework.getFireworkMeta();
 			FireworkType fireworkType = rank.getFireworkType();
 
 			for (FireworkEffect effect : fireworkType.getEffects()) {
@@ -56,8 +60,26 @@ public class JoinListener implements Listener {
 			}
 
 			fireworkMeta.setPower(fireworkType.getPower());
-
 			firework.setFireworkMeta(fireworkMeta);
+
+			Particle spiralParticle = fireworkType.getSpiralParticle();
+
+			if (spiralParticle == null) {
+				continue;
+			}
+
+			new BukkitRunnable() {
+
+				double angleDegrees = 0;
+
+				@Override
+				public void run() {
+					double angleRadians = Math.toRadians(angleDegrees);
+					playerWorld.spawnParticle(spiralParticle, firework.getLocation().add(-Math.cos(angleRadians), 0, Math.sin(angleRadians)), 1);
+
+					angleDegrees += 5;
+				}
+			}.runTaskTimer(plugin, 0L, 1L);
 		}
 	}
 }
