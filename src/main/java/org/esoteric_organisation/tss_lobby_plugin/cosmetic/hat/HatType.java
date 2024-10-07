7ecb9bcb8c28996291ad6d2b30ef03ffeac461e9
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2024-10-07 18:14:34 +0100

    Fix spelling issue

--- src/main/java/org/esoteric_organisation/tss_lobby_plugin/cosmetic/hat/HatType.java
+++ src/main/java/org/esoteric_organisation/tss_lobby_plugin/cosmetic/hat/HatType.java
@@ -9,7 +9,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
 public enum HatType implements Cosmetic {
-  TOPHAT(new TranslatableItemStack(Material.PLAYER_HEAD, Message.TOP_HAT_DISPLAY_NAME, Message.TOP_HAT_DESCRIPTION)),
+  TOP_HAT(new TranslatableItemStack(Material.PLAYER_HEAD, Message.TOP_HAT_DISPLAY_NAME, Message.TOP_HAT_DESCRIPTION)),
   TIGER(new TranslatableItemStack(Material.PLAYER_HEAD, Message.TIGER_HAT_DISPLAY_NAME, Message.TIGER_HAT_DESCRIPTION)),
   DOG(new TranslatableItemStack(Material.PLAYER_HEAD, Message.DOG_HAT_DISPLAY_NAME, Message.DOG_HAT_DESCRIPTION));
 
